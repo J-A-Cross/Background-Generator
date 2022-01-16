0190e260f6a1e2966637774f1ddddf6858a0a23a
J-A-Cross <jamcro007@gmail.com>
2022-01-17 08:28:27 +1300

    Adjust text color depending on bg
    Added functions to calculate the relative luminance and contrast between both background color and each color in relation to text color, flipping color when hard to see.

--- script.js
+++ script.js
@@ -15,6 +15,28 @@ function htmlButtonEvent(){
 	})
 }
 
+function contrastRatio(rgba, rgbb) {
+	let numerator = calcRelativeLuminance(rgba) + 0.05
+	let denominator = calcRelativeLuminance(rgbb) + 0.05
+	if (numerator < denominator){
+		return (denominator/numerator).toFixed(2)
+	}
+	return (numerator/denominator).toFixed(2)
+}
+
+function calcRelativeLuminance(rgb){
+	let rgbValues = (rgb.match(/[0-9]{3}/g))
+	rgbValues.forEach((value,pos) => {
+		if (value <= 10){
+			rgbValues[pos] = value/3294
+		} else {
+			rgbValues[pos] = Math.pow((value/269 + 0.0513),2.4)
+		}
+	})
+	luminance = (0.2126*rgbValues[0]+.7152*rgbValues[1]+.0722*rgbValues[2])
+	return luminance
+}
+
 function checkValidHex(leftColour, rightColour){
 	console.log(leftColour, rightColour)
 	lValid = false
@@ -41,6 +63,54 @@ function checkValidHex(leftColour, rightColour){
 	return (lValid && rValid);
 }
 
+function verifyThreeDigitRGB(rgb) {
+	let rgbValues = (rgb.match(/[0-9]{1,3}/g))
+	rgbValues.forEach((value,pos) => {
+		while (rgbValues[pos].length < 3){
+			rgbValues[pos] = '0'+rgbValues[pos]
+		}
+	})
+	return ('('+rgbValues[0]+','+rgbValues[1]+','+rgbValues[2]+')')
+}
+
+
+function toggleColor(){
+	elements = document.getElementsByClassName('txt')
+	for (item of elements) {
+		item.classList.toggle('white')
+	}
+}
+
+function getTextColour(){
+	if (document.getElementsByClassName('white').length == 4) {
+		console.log("IS CURRENT WHITE")
+		return '(255,255,255)'
+	}
+	return '(000,000,000)'
+}
+
+function textContrastAdjustment(leftColour,rightColour){
+	rgba = hexToRgb(leftColour)
+	console.log(rgba)
+	rgbb = hexToRgb(rightColour)
+	textColour = getTextColour()
+	console.log(textColour)
+	console.log('Contrast ratio between sides = ', contrastRatio(rgba,rgbb))
+	let contrastA = contrastRatio(textColour,rgba)
+	console.log('Contrast ratio text to color a: ',contrastA)
+	let contrastB = contrastRatio(textColour,rgbb)
+	console.log('Contrast ratio text to color b: ',contrastB)
+	if ((parseInt(contrastA)+parseInt(contrastB))/2 < 2) {
+		toggleColor()
+		console.log('COLOUR SWAPPED')
+	}
+}
+
+function hexToRgb(hex) {
+	var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+	return verifyThreeDigitRGB('('+parseInt(result[1], 16)+','+parseInt(result[2], 16)+','+parseInt(result[3], 16)+')')
+  }
+
 function changeButtonEventText(){
 	change = document.getElementById('select')
 	change.addEventListener("click", function(){
@@ -49,6 +119,7 @@ function changeButtonEventText(){
 		isValidInput = (checkValidHex(document.getElementById('leftColour').value, document.getElementById('rightColour').value));
 		if (isValidInput){
 			document.body.style.background = "linear-gradient(to right, "+currentLeftColour+" , "+currentRightColour+")";
+			textContrastAdjustment(document.getElementById('leftColour').value,document.getElementById('rightColour').value)
 		document.getElementById('leftColour').value = '';
 		document.getElementById('rightColour').value = '';	
 		document.getElementById('leftColour').placeholder = '#000000';
@@ -62,6 +133,9 @@ function changeButtonEventText(){
 function changeButtonEventWheel(){
 	change = document.getElementById('select')
 	change.addEventListener("click", function(){
+
+		textContrastAdjustment(document.getElementById('leftColour').value,document.getElementById('rightColour').value)
+
 		var h3 = document.getElementById('h3');
 		h3.innerText = ''
 		currentLeftColour = document.getElementById('leftColour').value;
@@ -139,4 +213,4 @@ rb2.addEventListener("click", function(){
 
 	changeButtonEventWheel()
 	htmlButtonEvent()
-})	
+})
